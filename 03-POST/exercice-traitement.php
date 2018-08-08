3acbd6ec795dcc6dda234ceb4ca689e34e18738b
Fabrice Domoison <39194172+Fabgraph@users.noreply.github.com>
2018-08-08 16:51:01 +0200

    commit04
    c04

--- 03-POST/exercice-traitement.php
+++ 03-POST/exercice-traitement.php
@@ -3,13 +3,24 @@ $rempli = '';
 
 // var_dump($_POST);
 
-if (!empty($_POST)) {
+if (!empty($_POST)) { // est équivalent à if ($_POST) : signifie que $_POST n'est pas vide, donc que le formulaire a été soumis
 
     $rempli = '<p>Ville : ' . $_POST['ville'] . '</p>';
     $rempli .= '<p>Code postal : ' . $_POST['code_postal'] . '</p>';
     $rempli .= '<p>Adresse : ' . $_POST['adresse'] . '</p>';
 }
-
-echo $rempli;
 ?>
 
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Votre formulaire</title>
+</head>
+<body>
+    <h1>Vous avez indiqué :</h1>
+    <?php echo $rempli; ?>
+</body>
+</html>
