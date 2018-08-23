344bb56ffdcb6bbd86f0d45da505e5b31ec8c52a
Fabrice Domoison <39194172+Fabgraph@users.noreply.github.com>
2018-08-23 17:02:06 +0200

    commit07
    c07

---
+++ 07-site/connexion.php
@@ -0,0 +1,55 @@
+<?php
+require_once 'inc/init.inc.php';
+// debug($_POST);
+
+// 1- traitement du formulaire :
+if(!empty($_POST)) {  // si le formulaire est soumis
+
+    // validation des champs :
+    if (!isset($_POST['pseudo']) || empty($_POST['pseudo'])) $contenu .= '<div class="bg-danger">Le pseudo est requis.</div>';
+
+    if (!isset($_POST['mdp']) || empty($_POST['mdp'])) $contenu .= '<div class="bg-danger">Le mot de passe est requis.</div>';
+
+    if (empty($contenu)) {  // s'il n'y a pas d'erreur sur le formulaire
+        $membre = executeRequete("SELECT * FROM membre WHERE pseudo = :pseudo AND mdp = :mdp", array(':pseudo' => $_POST['pseudo'], ':mdp' => $_POST['mdp']));
+
+        if ($membre->rowCount() > 0) {  // si le nombre de ligne est supérieur à 0, alors le login et le mdp existent ensemble en BDD
+            // on crée une session avec les informations du membre :
+                $informations = $membre->fetch(PDO::FETCH_ASSOC);  // on fait un fetch pour transformer l'objet $membre en array associatif qui contient en indices le nom de tous les champs de la requête
+                debug($informations);
+
+                $_SESSION['membre'] = $informations; // nous créons une session avec les infos du membre qui proviennent de la BDD
+
+                header('location:profil.php');
+                exit();  // on redirige l'internaute vers sa page de profil, et on quitte ce script avec la fonction exit()
+
+        } else {
+            // sinon c'est qu'il y a erreur sur les identifiants (ils n'existent pas ou pas pour le même membre)
+            $contenu .= '<div class="bg-danger">Erreur sur les identifiants.</div>';
+        }
+
+    }  // fin du if (empty($contenu))
+    
+
+} // fin du if (!empty($_POST))
+
+
+//------------------ AFFICHAGE ------------------
+require_once 'inc/haut.inc.php';
+?>
+    <h1 class="mt-4">Connexion</h1>
+    <?php echo $contenu; ?>
+
+    <form method="post" action="">
+        <label for="pseudo">Pseudo</label><br>
+        <input type="text" name="pseudo" id="pseudo" value=""><br><br>
+
+        <label for="mdp">Mot de passe</label><br>
+        <input type="password" name="mdp" id="mdp" value=""><br><br>
+
+        <input type="submit" value="Se connecter" class="btn">
+    </form>
+
+
+<?php
+require_once 'inc/bas.inc.php';
---
+++ 07-site/inc/init.inc.php
@@ -0,0 +1,23 @@
+<?php
+/* Ce fichier sera inclus dans TOUS les scripts (hors inc eux m�mes) pour initialiser les �l�ments suivants :
+- connexion � la BDD
+- cr�er ou ouvrir une session
+- d�finir le chemin absolu du site (comme dans wordpress)
+- inclure le fichier fonctions.inc.php � la fin de ce fichier pour l'embarquer dans tous les scripts. */
+
+// connexion � la BDD
+$pdo = new PDO('mysql:host=localhost;dbname=site_commerce', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING, PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+
+// cr�er ou ouvrir une session
+session_start();
+
+// d�finir le chemin absolu du site (comme dans wordpress)
+define('RACINE_SITE', '/PHP/07-site/'); //cette constante servira � cr�er les chemins absolus utilis�s dans haut.inc.php car ce fichier sera inclus dans des scripts qui se situent dans des dossiers diff�rents du site. On ne peut donc pas faire de chemin relatif dans ce fichier.
+
+// Variables d'affichage :
+$contenu = '';
+$contenu_gauche = '';
+$contenu_droite = '';
+
+// Inclusion du fichier fonctions.inc.php :
+require_once('fonctions.inc.php');
